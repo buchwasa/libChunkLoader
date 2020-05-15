be5c088d45485a7b09bea979892d325a1d48a051
Wumpotamus <17461354+Wumpotamus@users.noreply.github.com>
2020-05-14 23:55:09 -0400

    Initial commit.

---
+++ src/wumpotamus/chunkloader/RegionLoader.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace wumpotamus\chunkloader;
+
+use pocketmine\level\Level;
+
+final class RegionLoader{
+	
+	public static function onChunkGenerated(Level $level, int $chunkX, int $chunkZ, callable $callback) : void{
+		if($level->isChunkPopulated($chunkX, $chunkZ)){
+			$callback();
+			return;
+		}
+		$level->registerChunkLoader(new InternalChunkLoader($level, $chunkX, $chunkZ, $callback), $chunkX, $chunkZ, true);
+	}
+}
